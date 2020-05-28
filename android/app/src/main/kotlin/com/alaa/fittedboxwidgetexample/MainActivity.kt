41ef591bdcd22602ecba88faa87efb28a42a196f
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 16:41:33 +0530

    FittedBox widget example

---
+++ android/app/src/main/kotlin/com/alaa/fittedboxwidgetexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alaa.fittedboxwidgetexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:fittedboxwidgetexample/my_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'FittedBox widget Example',
+    home: MyHome(),
+  ));
+}
